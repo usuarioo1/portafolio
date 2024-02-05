c96f6e83e11f3f837fb2b08863ef61d553d47e92
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2024-02-04 22:53:32 -0300

    add home

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,7 +10,7 @@ export const AppRoutes = () => {
         <Routes>
 
             <Route path="/" element={<Layout />}>
-                <Route path='/home' element={<Home />} />
+                <Route path='/' element={<Home />} />
 
 
 
